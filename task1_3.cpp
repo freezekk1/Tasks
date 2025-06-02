32008ad34a26be5708a29aa759dc8d2a856c420d
freezekk1 <shirinkinegor2007@mail.ru>
2025-06-02 18:36:09 +0300

    Add files via upload

---
+++ task1_3.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: " << endl;
+    cin >> number;
+
+    cout << "You have entered a number: " << number << endl;
+}
+
