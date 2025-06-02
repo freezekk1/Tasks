28fe4331bbbef1637bdff8d1e8cca22b0fc2a27b
freezekk1 <shirinkinegor2007@mail.ru>
2025-06-02 19:46:58 +0300

    Add files via upload

---
+++ task1_43.cpp
@@ -0,0 +1,24 @@
+﻿//Даны два числа. Найти среднее арифметическое и среднее геометрическое их
+//модулей.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a, b; 
+
+    cout << "Enter 2 numbers: ";
+    cin >> a >> b;
+
+    double absA = abs(a);
+    double absB = abs(b);
+
+    double arithmetic = (absA + absB) / 2;
+    double geometric = sqrt(absA * absB);
+
+    cout << "Среднее арифметическое модулей: " << arithmetic<< endl;
+    cout << "Среднее геометрическое модулей: " << geometric << endl;
+
+}
+
