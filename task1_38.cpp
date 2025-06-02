5e58163cfdddd7a0238aaa338c30045f3bc3bbea
freezekk1 <shirinkinegor2007@mail.ru>
2025-06-02 19:37:48 +0300

    Add files via upload

---
+++ task1_38.cpp
@@ -0,0 +1,21 @@
+﻿// Даны основания и высота равнобедренной трапеции. Найти ее периметр.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a, b, h;
+
+    cout << "Enter a: " << endl;
+    cin >> a;
+    cout << "Enter b: " << endl;
+    cin >> b;
+    cout << "Enter h: " << endl;
+    cin >> h;
+
+    double side = sqrt(pow((b - a) / 2, 2) + (h * h));
+
+    double p = a + b + side * 2;
+    cout << "Perimeter: " << p << endl;
+}
