326d55d1bcd6fefaf2c16aec0bf4b3adc58f8f45
freezekk1 <shirinkinegor2007@mail.ru>
2025-06-02 21:30:07 +0300

    Add files via upload

---
+++ task3_4.cpp
@@ -0,0 +1,24 @@
+﻿/*Вычислить значение логического выражения при следующих значениях логи-
+ческих величин X, Y и Z: X = Истина, Y = Истина, Z = Ложь:
+
+а) не X и Y; б) X или не Y; в) X или Y и Z.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool X = true;
+    bool Y = true;
+    bool Z = false;
+
+    bool a = !X && Y;
+    bool b = X || !Y;
+    bool c = X || Y && Z;  
+
+    cout << "а) !X && Y = " << a << endl;
+    cout << "б) X || !Y = " << b << endl;
+    cout << "в) X || Y && Z = " << c << endl;
+
+    return 0;
+}
